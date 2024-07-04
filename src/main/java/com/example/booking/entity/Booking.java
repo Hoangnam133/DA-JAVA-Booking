37d7eb78fb546d68e08b3185d595671d7606ee5c
Hoangnam133 <012003hoangnam@gmail.com>
2024-07-04 14:32:41 +0700

    thu xem con xung dot ko

--- src/main/java/com/example/booking/entity/Booking.java
+++ src/main/java/com/example/booking/entity/Booking.java
@@ -1,6 +1,7 @@
 package com.example.booking.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
